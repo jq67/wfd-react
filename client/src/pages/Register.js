712df9a96baeb2f13d8bdc3bbc5da45ef0cce68c
Julian Quan Fun <julianquanfun@gmail.com>
2022-08-09 13:32:11 -0400

    fixes

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -6,12 +6,21 @@ import Auth from '../utils/auth';
 
 const SignupForm = () => {
     // set initial form state
-    const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+    const [formUsername, setFormUsername] = useState('')
+    const [formEmail, setFormEmail] = useState('')
+    const [formPassword, setFormPassword] = useState('')
   
-    const formChange = (event) => {
-      const { name, value } = event.target;
-      setUserFormData({ ...userFormData, [name]: value });
-    };
+    const nameChange = (event) => {
+      setFormUsername(event.target.value)
+    }
+
+    const emailChange = (event) => {
+      setFormEmail(event.target.value)
+    }
+
+    const passChange = (event) => {
+      setFormPassword(event.target.value)
+    }
   
     const [ addUser, { error, data } ] = useMutation(CREATE_USER);
   
@@ -20,7 +29,7 @@ const SignupForm = () => {
   
       try {
         const { data } = await addUser({
-          variables: { ...userFormData },
+          variables: { username: formUsername, email: formEmail, password: formPassword },
         });
   
         if (!data) {
@@ -42,15 +51,15 @@ const SignupForm = () => {
               <form className="form signup-form" onSubmit={handleFormSubmit}>
                   <div className="pb-3 m-4 cardbg">
                       <label htmlFor="username-signup" className="form-label">Username</label>
-                      <input className="form-control text-center shadow w-75 mx-auto" type="text" id="username-signup"></input>
+                      <input className="form-control text-center shadow w-75 mx-auto" type="text" id="username-signup" onChange={nameChange}></input>
                   </div>
                   <div className="pb-3 m-4 cardbg">
                       <label htmlFor="email-signup" className="form-label">Email</label>
-                      <input className="form-control text-center shadow w-75 mx-auto"type="text" id="email-signup" onChange={formChange}></input>
+                      <input className="form-control text-center shadow w-75 mx-auto"type="text" id="email-signup" onChange={emailChange}></input>
                   </div>
                   <div className="pb-3 m-4 cardbg">
                       <label htmlFor="password-signup" className="form-label">Password</label>
-                      <input className="form-control text-center shadow w-75 mx-auto" type="password" id="password-signup" onChange={formChange}></input>
+                      <input className="form-control text-center shadow w-75 mx-auto" type="password" id="password-signup" onChange={passChange}></input>
                   </div>
                   <div className="pb-3 m-4">
                       <button className="btn btn-primary shadow" type="submit">Register</button>
